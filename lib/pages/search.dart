19e377b2b8f0a580fb80251e6b1ebc874a414546
4eug <debraheug@gmail.com>
2021-09-05 20:54:01 +0100

    On Search Page

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_redesign_concept_by_chidalu/widgets/constants.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key key}) : super(key: key);
@@ -10,6 +11,24 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: Stack(children: <Widget>[
+          Container(
+              color: Colors.blue,
+              width: double.infinity,
+              child: Opacity(
+                opacity: 0.5,
+                child: Image.asset(
+                  StyleGuide.searchBackground,
+                  height: 1000,
+                  width: 100,
+                  fit: BoxFit.fitHeight,
+                ),
+              ))
+        ]),
+      ),
+    );
   }
 }
---
+++ lib/widgets/constants.dart
@@ -0,0 +1,6 @@
+class StyleGuide {
+  static final String searchBackground = 'assets/images/search_background.png';
+  static final String homeBackground = 'assets/images/home_background.png';
+  static final String playlistBackground =
+      'assets/images/playlist_background.png';
+}
