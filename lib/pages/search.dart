768177300a4e5f4694671d045f7ca8d8ff21df92
4eug <debraheug@gmail.com>
2021-09-05 21:24:12 +0100

    Added Search Bar

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -16,19 +16,91 @@ class _SearchPageState extends State<SearchPage> {
         physics: NeverScrollableScrollPhysics(),
         child: Stack(children: <Widget>[
           Container(
-              color: Colors.blue,
-              width: double.infinity,
-              child: Opacity(
-                opacity: 0.5,
-                child: Image.asset(
-                  StyleGuide.searchBackground,
-                  height: 1000,
-                  width: 100,
-                  fit: BoxFit.fitHeight,
+            color: Colors.black87,
+            width: double.infinity,
+            child: Opacity(
+              opacity: 0.1,
+              child: Image.asset(
+                StyleGuide.searchBackground,
+                height: 1000,
+                width: 100,
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Padding(
+                  padding: EdgeInsets.symmetric(vertical: 30, horizontal: 10)),
+              Center(
+                child: Text(
+                  "Search",
+                  style: TextStyle(
+                    fontSize: 35,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
-              ))
+              ),
+              SizedBox(height: 40),
+              _searchBar()
+            ],
+          ),
         ]),
       ),
     );
   }
+}
+
+Widget _searchBar() {
+  return Container(
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.all(
+        Radius.circular(50),
+      ),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black,
+          blurRadius: 25,
+          offset: Offset(0, 5),
+          spreadRadius: -25,
+        ),
+      ],
+    ),
+    margin: EdgeInsets.only(bottom: 20),
+    child: TextField(
+      style: TextStyle(
+          fontSize: 20,
+          color: Colors.black,
+          letterSpacing: 0.24,
+          fontWeight: FontWeight.w500),
+      decoration: InputDecoration(
+        hintText: "Email address",
+        hintStyle: TextStyle(
+          color: Color(0xffA6B0BD),
+        ),
+        fillColor: Colors.white,
+        filled: true,
+        suffixIcon: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(
+            Icons.search,
+            color: Colors.greenAccent,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(1),
+          ),
+          borderSide: BorderSide(color: Colors.white),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(1),
+          ),
+          borderSide: BorderSide(color: Colors.white),
+        ),
+      ),
+    ),
+  );
 }
